b1fa20725c0af7163910dcf8cfd16b42005d47da
Saren Currie <saren@sarencurrie.com>
2015-02-17 09:28:01 +1300

    Added .vimrc

--- dotfiles-setup.zsh
+++ dotfiles-setup.zsh
@@ -4,6 +4,7 @@
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 # Make zsh the default shell
 chsh -s /bin/zsh
